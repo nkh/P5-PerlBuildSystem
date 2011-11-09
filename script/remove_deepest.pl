109543b64560831e548ccda71dc599c301c49271
Nadim Khemir <nadim.khemir@gmail.com>
2011-11-09 02:19:08 +0100

    CHANGED: made script much more efficient

--- script/remove_deepest.pl
+++ script/remove_deepest.pl
@@ -1,45 +1,32 @@
 
 use strict ;
 use warnings ;
+use v5.10 ;
 
 use File::Find;
 
 my $regex = $ARGV[0] or die "Missing regex";
 my $n = $ARGV[1] || 1 ;
 
-for (1 .. $n)
-	{
-	my $deepest = find_deepest($regex) ;
-	print "$deepest\n" ;
-	unlink $deepest or die "Can't unlink\n"
-	}
+use Tie::Array::Sorted ;
 
-# Euuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuurk!
-
-my $deepest_path = '' ;
-my $deepest = -1 ;
-my $filter = '' ;
-
-sub find_deepest
-{
-($filter) = @_ ;
-
-$deepest_path = '' ;
-$deepest = -1 ;
+tie my @found_files, 'Tie::Array::Sorted', sub {$_[0][0] <=> $_[1][0]} ;
 
 finddepth(\&wanted, '.');
 
-return $deepest_path ;
-}
+unlink  $_->[1] for @found_files ;
+
+#-------------------------------------------------------------------
 
 sub wanted 
 {
-my $depth = $File::Find::name =~ tr[/][/] ;
 
-if($File::Find::name =~ /$filter/o  && $depth > $deepest)
+if($File::Find::name =~ /$regex/o)
 	{
-	$deepest = $depth ;
-	$deepest_path = $File::Find::name ;
+	my $depth = $File::Find::name =~ tr[/][/] ;
+	push @found_files, [$depth, $File::Find::name] ;  
+	
+	shift @found_files if @found_files > $n ;
 	}
 }
 	    
